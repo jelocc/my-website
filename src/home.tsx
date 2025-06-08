e06ebce233c79ffba61048c74f1d032401322058
jelozz <118350308+jelozz@users.noreply.github.com>
2025-06-08 18:58:22 +0800

    added header without pic

--- src/home.tsx
+++ src/home.tsx
@@ -1,6 +1,23 @@
 const Home = () => {
-   return ( <>
+   return ( 
+   <>
+      <div className="py-20 md:py-28">
+         <div className ="container px-4 mx-auto">
+            <div className="flex flex-wrap xl:item-center -mx-4">
+               <div className="w-full md:w-1/2 px-4 mb-16 md:mb-0">
+                  <span className="inline-block py-px px-2 mb-4 text-xl leading-5 text-white  rounded-9xl"> Hello There</span>
+                  <h1 className="mb-6 text-3xl md:text-5xl lg:text-6xl leading-tight font-bold tracking-tight" data-config-id="auto-txt-14-2">I'm Ramil Angelo Rosal</h1>
+                  <h2 className="mb-8 text-lg md:text-xl text-white-500 font-medium" data-config-id="auto-txt-15-2">A Software Engineer that builds responsive and cool applications.</h2>
+               </div>
+               <div className="w-full md:w-1/2 px-4">
+                  <div className="relative mx-auto md:mr-0 max-w-max">
 
+                  </div>
+               </div>
+               <div className="w-full md:w-1/2 px-4"></div>
+            </div>
+         </div>
+      </div>
     </>
    )
 }
